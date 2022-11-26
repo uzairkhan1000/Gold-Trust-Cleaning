de8952d4637d9ac059957ded97110ee8ed166093
uzairkhan1000 <uk5598060@gmail.com>
2022-11-26 13:33:43 +0500

    Update

--- resources/views/layouts/default_layout.blade.php
+++ resources/views/layouts/default_layout.blade.php
@@ -5,6 +5,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
+        <!-- This is Uzair Khan yeeyy -->
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
         <!-- Owl Carousel CSS -->
