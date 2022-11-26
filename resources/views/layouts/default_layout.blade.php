9884fd5ab61c2f7136659d9d2217c5ca8e414964
uzairkhan1000 <uk5598060@gmail.com>
2022-11-26 14:11:24 +0500

    Update 3

--- resources/views/layouts/default_layout.blade.php
+++ resources/views/layouts/default_layout.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-        <!-- This is Uzair Khan -->
+        <!-- This is Uzair Khan Swati -->
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
         <!-- Owl Carousel CSS -->
